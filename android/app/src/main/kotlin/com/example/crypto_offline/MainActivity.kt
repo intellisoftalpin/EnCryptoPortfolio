c06b1abf504ae0922dc547e24dab47f1da7c0a24
intellisoftalpin <info@intellisoftalpin.com>
2023-06-12 17:28:34 +0300

    [FIX] [EP-262] Name of zip file is displayed in two lines

--- android/app/src/main/kotlin/com/example/crypto_offline/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/crypto_offline/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.encryptoportfolio.app
+package com.intellisoftalpin.encryptoportfolio
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
